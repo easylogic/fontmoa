cdbd6cee38ed92518205192c950220c7e0322a0f
박진호(Jinho Park)/Vroong <jh.park3@humaxdigital.com>
2017-08-08 21:34:40 +0900

    add css text-shadow sample

--- src/component/cssmanager/index.js
+++ src/component/cssmanager/index.js
@@ -13,7 +13,16 @@ class CssManager extends Component {
 
     return (
         <TabItem ref="tabItem"  id={this.props.id}  active={this.props.active}>
-          
+          <div  className="cm-content">
+
+              <p><a href="https://en.wikipedia.org/wiki/Red">사랑</a></p>
+              <p><a href="https://en.wikipedia.org/wiki/Blue">평화</a></p>
+              <p><a href="https://en.wikipedia.org/wiki/Yellow">정의의</a></p>
+              <p><a href="https://en.wikipedia.org/wiki/Green">이름으로</a></p>
+              <p><a href="https://en.wikipedia.org/wiki/Orange_(colour)">널 용서하지</a></p>
+              <p><a href="https://en.wikipedia.org/wiki/Violet_(color)">않겠다.</a></p>
+
+          </div>
         </TabItem>
     );
   }
